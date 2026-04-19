f1231cd127529495155d399e85af829dab4bc2d7
nlaot-ai <nlaot@ucenfotec.ac.cr>
2026-04-18 23:15:45 -0600

    [feat]: Agregar lógica de validación en formulario de inicio de sesión

---
+++ js/iniciar_sesion.js
@@ -0,0 +1,27 @@
+console.log("JS conectado");
+document.addEventListener("DOMContentLoaded", function () {
+  const btn_login = document.getElementById("boton_login");
+  const email_input = document.getElementById("email");
+  const password_input = document.getElementById("password");
+
+  btn_login.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const email = email_input.value.trim();
+    const password = password_input.value.trim();
+
+    if (email === "" || password === "") {
+      Swal.fire({
+        icon: "warning",
+        title: "Campos incompletos",
+        text: "Por favor, completa todos los campos",
+      });
+      return;
+    }
+    Swal.fire({
+      icon: "success",
+      title: "Bienvenido",
+      text: "Los datos ingresados son correctos.",
+    });
+  });
+});
